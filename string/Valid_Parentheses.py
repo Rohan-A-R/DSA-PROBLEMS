aa839094bc28477d9382ae256a30eeeb79e035c9
Rohan <rohanflash27@gmail.com>
2025-01-22 22:23:18 +0530

    first

---
+++ string/Valid_Parentheses.py
@@ -0,0 +1,26 @@
+def Valid_paranthisi(s):
+    hash_map={
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    stack=[]
+
+    for char in s:
+        if char in hash_map.values():
+            stack.append(char)
+        elif char in hash_map:
+            if not stack or stack[-1]!=hash_map[char]:
+                return False
+            stack.pop()
+
+        else:
+            return False
+        
+    return not stack
+
+s = "[{()}]"
+print(Valid_paranthisi(s))
+
+
+
