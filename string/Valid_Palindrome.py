541d26d42117b64cd1d26ec4c84a50fc763f1e44
Rohan <rohanflash27@gmail.com>
2025-01-23 09:25:09 +0530

    first

---
+++ string/Valid_Palindrome.py
@@ -0,0 +1,18 @@
+def valid_palindrome(s):
+    filtered_chars=[]
+    for char in s:
+        if char.isalnum():
+            filtered_chars.append(char.lower())
+    s=''.join(filtered_chars)
+
+    left,right=0,len(s)-1
+    while left<right:
+        if s[left]!=s[right]:
+            return False
+        left+=1
+        right-=1
+    return True
+
+
+s = "race a car"
+print(valid_palindrome(s))
