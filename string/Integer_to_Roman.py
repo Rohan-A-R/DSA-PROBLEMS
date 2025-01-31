983b9670ae2128ce355c4c24fb10ffe455c5b9cb
Rohan <rohanflash27@gmail.com>
2025-01-31 20:10:41 +0530

    first

---
+++ string/Integer_to_Roman.py
@@ -0,0 +1,36 @@
+# 1st method
+def convert_to_roman(n):
+    values = [
+        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+        (1, "I")
+    ]
+    roman=""
+
+    for values,symbol in values:
+        while n>=values:
+            roman+=symbol
+            n-=values
+
+    return roman
+n=232
+print(convert_to_roman(n))
+
+# 2nd method(better)
+def convert_to_roman_2nd_method(n):
+    values = [
+        (1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+        (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+        (10, "X"), (9, "IX"), (5, "V"), (4, "IV"),
+        (1, "I")
+    ]
+    roman=""
+    for value,symbol in values:
+        count=n//value
+        roman+=symbol*count
+        n%=value
+    return roman
+n=232
+print(convert_to_roman_2nd_method(n))
+
